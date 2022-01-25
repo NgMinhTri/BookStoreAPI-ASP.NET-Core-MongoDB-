8849c81944c6c62f417af9bfdf7be68f6266fdc7
Minh Tri Nguyen <minhtripro2903@gmail.com>
2022-01-25 14:44:55 +0700

    Tạo Cache Thời gian đơn giản sử dụng DistributeCache Redis

--- BookStoreApi/Program.cs
+++ BookStoreApi/Program.cs
@@ -17,6 +17,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost";
+    options.InstanceName = "SampleInstance";
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
